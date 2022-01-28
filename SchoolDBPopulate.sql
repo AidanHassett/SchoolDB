1fe8088ebdfa4c689689f185fe66a963b9ee2f8d
Aidan Fiadh de Carteret Hassett <47570113+AidanHassett@users.noreply.github.com>
2022-01-28 14:39:58 +0000

    Correct a typo

--- SchoolDBPopulate.sql
+++ SchoolDBPopulate.sql
@@ -22,7 +22,7 @@ INSERT INTO studentNeed (studentID, needID) VALUES
     (4, 2);
     
 INSERT INTO staff (fullName, occupation, email, emergencyContactPhone, emergencyContactName, emergencyContactRelation) VALUES
-	('Michael Smith', 'History Teacher', 'ms_hist@school.com', 07654321234, 'Susan Smith', 'Wife'),
+	('Michael Smith', 'History Teacher', 'ms_hist@school.com', '07654321234', 'Susan Smith', 'Wife'),
 	('Michelle Hart', 'Head of Maths', 'mh_hom@school.com', '07386291042', 'Sam Hart', 'Son'),
 	('Harry Hannan', 'English Teacher', 'hh_eng@school.com', '07295718834', 'Matt Hannan', 'Brother'),
 	('Hope Kiston', 'Head of Science', 'hk_hos@school.com', '07333578901', 'Mary Miller', 'Aunty'),
