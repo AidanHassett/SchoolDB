b6107a5d119a25947611366cb3c0130d5564cca9
AidanHassett <aidanhassett.personal@gmail.com>
2022-01-28 13:18:26 +0000

    Added staff and class data to SchoolDBPopulate.sql

--- SchoolDBPopulate.sql
+++ SchoolDBPopulate.sql
@@ -20,3 +20,17 @@ INSERT INTO studentNeed (studentID, needID) VALUES
     (2, 4),
     (4, 1),
     (4, 2);
+    
+INSERT INTO staff (fullName, occupation, email, emergencyContactPhone, emergencyContactName, emergencyContactRelation) VALUES
+	('Michael Smith', 'History Teacher', 'ms_hist@school.com', 07654321234, 'Susan Smith', 'Wife'),
+	('Michelle Hart', 'Head of Maths', 'mh_hom@school.com', '07386291042', 'Sam Hart', 'Son'),
+	('Harry Hannan', 'English Teacher', 'hh_eng@school.com', '07295718834', 'Matt Hannan', 'Brother'),
+	('Hope Kiston', 'Head of Science', 'hk_hos@school.com', '07333578901', 'Mary Miller', 'Aunty'),
+	('Kane Manion', 'Head Teacher', 'km_ht@school.com', '07376769001', 'Katherine Manion', 'Wife');
+
+INSERT INTO class (teacher, classSubject, timePeriod, dayOfWeek, room) VALUES
+	(1, 'History', 1, 1, 'H1'),
+    (2, 'Maths', 2, 1, 'M1'),
+    (2, 'Computer Science', 4, 1, 'C1'),
+    (3, 'Drama', 4, 1, 'Main Hall'),
+    (4, 'Physics', 1, 2, 'Lab 1');
