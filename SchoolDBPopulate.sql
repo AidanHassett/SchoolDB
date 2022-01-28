30132e2706b10428c582a5bfd8b6d9ee8de65000
AidanHassett <aidanhassett.personal@gmail.com>
2022-01-28 11:35:49 +0000

    Added data for students to SchoolDBPopulate.sql

---
+++ SchoolDBPopulate.sql
@@ -0,0 +1,7 @@
+USE SchoolDB;
+
+INSERT INTO student (fullName, yearGroup, address, emergencyContactPhone, emergencyContactName, emergencyContactRelation) VALUES
+	('Bob Smith', 7, '17 Yellow Road', '07654321234', 'John Smith', 'Father'),
+	('Jimmy Stevens', 9, '16 Acre Lane, SW4 4BZ', '07896324185', 'Jenny McCauley', 'Carer'),
+    ('Karen James', 8, '13 Ash Drive', '07425836184', 'Christian James', 'Father'),
+    ('Danny Jenson', 11, '124 Acre Lane', '07892735168', 'Paula Jenson', 'Mother');
